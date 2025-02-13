2f8d5f4168cbad6d1224bfe14c8279de72a53608
aub <aub@macbookpro.mynetworksettings.com>
2025-02-13 14:03:45 -0500

    sdl hello world

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Video Player
 My implementation of Dranger's ffmpeg/dsl tutorial http://dranger.com/ffmpeg/ffmpeg.html
+
+To compile rn:
+
+```bash
+clang -lSDL2 -o main.out main.c
+```
---
+++ main.c
@@ -0,0 +1,54 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    } else {
+        printf("SDL_Init successful\n");
+    }
+
+    SDL_Window *win = SDL_CreateWindow("Hello SDL", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (!win) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    } else {
+        printf("SDL_CreateWindow successful\n");
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    } else {
+        printf("SDL_CreateRenderer successful\n");
+    }
+
+    // Event loop
+    SDL_Event e;
+    int quit = 0;
+    while (!quit) {
+        // Handle events
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            }
+        }
+
+        // Render a red rectangle
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);  // Red color
+        SDL_RenderClear(renderer);  // Clear screen with red
+        SDL_RenderPresent(renderer);  // Update the window
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
+    return 0;
+}
+
