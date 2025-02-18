bed2955aeab00c3af13133513af3ddf70bb3f211
Aubrey Wahl <aub@macbookpro.mynetworksettings.com>
2025-02-18 14:18:04 -0500

    refactor: render function

--- main.c
+++ main.c
@@ -4,9 +4,28 @@
 #include <stdio.h>
 
 #define SDL_COLOR_YELLOW (SDL_Color){255, 255, 0, 255}
+#define COOL_FONT "/System/Library/Fonts/Supplemental/Comic Sans MS Bold.ttf"
+
+TTF_Font* cool_font = NULL;
+
+void RenderFrame(SDL_Renderer* renderer) {
+    SDL_Surface* textSurface = TTF_RenderText_Solid(cool_font, "Hello!", SDL_COLOR_YELLOW);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    SDL_Rect rect = {
+        .x = 0, .y = 0, .w = textSurface->w, .h = textSurface->h
+    };
+
+    // Render a red rectangle
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);  // Red color
+    SDL_RenderClear(renderer);  // Clear screen with red
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+    SDL_RenderPresent(renderer);  // Update the window
+    SDL_FreeSurface(textSurface);
+    SDL_DestroyTexture(texture);
+}
 
 int main(int argc, char *argv[]) {
-    printf("hi!!!!\n");
+
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("SDL_Init Error: %s\n", SDL_GetError());
         return 1;
@@ -14,6 +33,12 @@ int main(int argc, char *argv[]) {
 
     if (TTF_Init() == -1) {
         printf("TTF_Init Error: %s\n", TTF_GetError());
+        return 1;
+    }
+
+    cool_font = TTF_OpenFont(COOL_FONT, 24);
+    if (cool_font == NULL) {
+        printf("Oops, couldn't load font %s!\n", COOL_FONT);
         return 1;
     }
 
@@ -32,19 +57,6 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // This is not portable.
-    TTF_Font* sans = TTF_OpenFont("/System/Library/Fonts/NewYorkItalic.ttf", 24);
-    if (sans == NULL) {
-        printf("Oops, not text!\n");
-        return 1;
-    }
-
-    SDL_Surface* textSurface = TTF_RenderText_Solid(sans, "Hello!", SDL_COLOR_YELLOW);
-
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, textSurface);
-    SDL_Rect rect = {
-        .x = 0, .y = 0, .w = textSurface->w, .h = textSurface->h
-    };
 
     // Event loop
     SDL_Event e;
@@ -56,16 +68,9 @@ int main(int argc, char *argv[]) {
                 quit = 1;
             }
         }
-
-        // Render a red rectangle
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);  // Red color
-        SDL_RenderClear(renderer);  // Clear screen with red
-        SDL_RenderCopy(renderer, texture, NULL, &rect);
-        SDL_RenderPresent(renderer);  // Update the window
+        RenderFrame(renderer);
     }
 
-    SDL_FreeSurface(textSurface);
-    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(win);
     SDL_Quit();
